02f9f1800fd80ee4be28df7563c8288b3fc466c2
Pedro Cruz <pedrohlcruz@gmail.com>
2020-01-21 17:22:00 -0300

    modified:   RaspberryPiZeroW_Script.py
        •Fixed the issue with the wakeup function;

--- RaspberryPiZeroW_Script.py
+++ RaspberryPiZeroW_Script.py
@@ -186,22 +186,26 @@ class RaspberryPi():
         if self.wake_lights_on:
 
             # Get the current time
-            time = self.__get_current_time()
+            current_time = self.__get_current_time()
 
             # parse the time on the json file
             self.wake_lights_time = self.wake_lights_time.split(':')
 
             # If the hours match, check the minutes
-            if time['hour'] == int(self.wake_lights_time[0]):
+            if current_time['hour'] == int(self.wake_lights_time[0]):
 
                 # Check the minutes
-                if time['minutes'] == int(self.wake_lights_time[1]):
+                if current_time['minutes'] == int(self.wake_lights_time[1]):
 
                     # turn on the lights
                     self.__control_lights(rbg_values = {'R':255, 'G':255, 'B':255})
 
                     # update the server
                     self.__make_post_request(header='On', payload=True)
+                    self.__make_post_request(header='Wake Time', payload={'wake':False})
+                    
+                    # Wait for the server to update
+                    time.sleep(5)
         
 
 # Instanciate the raspberry pi class
